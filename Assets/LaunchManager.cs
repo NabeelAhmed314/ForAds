e3965eb19c071daf563a65309a1451ab822fe35d
Nabeel Ahmed <nabeelrajpoot314@gmail.com>
2020-09-07 12:03:42 +0500

    first

---
+++ Assets/LaunchManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class LaunchManager : MonoBehaviourPunCallbacks
+{
+    public void ConnectedToPhotonServer()
+    {
+        if (!PhotonNetwork.IsConnected)
+        {
+            PhotonNetwork.ConnectUsingSettings();
+        }
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log(PhotonNetwork.NickName + "  is Connected to server");
+    }
+
+    public override void OnConnected()
+    {
+        Debug.Log("Connected to Internet");
+    }
+}
---
+++ Assets/PlayerNameInput.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+
+
+
+public class PlayerNameInput : MonoBehaviour
+{
+    public void SetPlayerName(string playerName)
+    {
+        if (string.IsNullOrEmpty(playerName))
+        {
+            Debug.Log("Player Name is Empty");
+            return;
+        }
+        PhotonNetwork.NickName = playerName;
+    }
+
+}
